a37bdfd1258e1f63e0e19d6ebdc371109340604f
David <44806074+Hamshank@users.noreply.github.com>
2021-03-14 15:24:51 +0100

    added tank hpp

---
+++ ufo/include/kmint/ufo/states/tank/roamTankState.h
@@ -0,0 +1,16 @@
+//
+// Created by David van der Zalm on 13-3-2021.
+//
+
+#ifndef KMINT_DAVID_JEROEN_UFO_ROAMTANKSTATE_H
+#define KMINT_DAVID_JEROEN_UFO_ROAMTANKSTATE_H
+
+#include "tankBaseState.h"
+
+class roamTankState : public tankBaseState {
+public:
+    void tick(tank& tank) override;
+};
+
+
+#endif //KMINT_DAVID_JEROEN_UFO_ROAMTANKSTATE_H
